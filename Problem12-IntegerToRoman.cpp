7a261593b10b09f3750f0d3cd7f07e845cede5c1
Cjh <cjh327@126.com>
2019-02-26 21:05:43 +0800

    Create Problem12-IntegerToRoman.cpp

---
+++ Problem12-IntegerToRoman.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman;
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string letters[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        for (int i = 0; i < 13; i++) {
+            while (num >= values[i]) {
+                roman += letters[i];
+                num -= values[i];
+            }
+        }
+        return roman;
+    }
+};
+
+static auto _=[]()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    return 0;
+}();
