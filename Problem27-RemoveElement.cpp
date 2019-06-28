85d9746a96795628194d2e9e157d1d1eae7f8985
CjhGm <cjh327@126.com>
2019-06-28 09:35:01 +0800

    Add problem 26

---
+++ Problem27-RemoveElement.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		if (nums.empty()) {
+			return 0;
+		}
+		int i = 0, j = 0;
+		while (j < nums.size()) {
+			if (val == nums[j]) {
+				j++;
+			}
+			else {
+				nums[i++] = nums[j++];
+			}
+		}
+		nums.erase(nums.begin() + i, nums.end());
+		return i;
+	}
+};
+
+int main() {
+	vector<int> nums{ 3,2,2,3 };
+	Solution sol;
+	cout << sol.removeElement(nums, 3) << endl;
+	for (int x : nums) {
+		cout << x << " ";
+	}
+	cout << endl;
+	return 0;
+}
